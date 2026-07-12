2b5684a88ebb9423dc1acf363c74433646a37ad9
Zahiraa <zahira_sa@hotmail.com>
2026-07-12 01:44:46 +0000

    les corrections d affichage

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -17,6 +17,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AuthorController extends AbstractController
 {
    
+    #[Route('/list', name: 'app_author_list_front', methods: ['GET'])]
+    public function listFront(AuthorRepository $authorRepository): Response
+    {
+        return $this->render('author/listFront.html.twig', [
+            'authors' => $authorRepository->findAll(),
+        ]);
+    }
+
    #[Route('/{id}', name: 'app_author_show_front', methods: ['GET'])]
     public function show(Author $author): Response
     {
--- src/Controller/ContenuNumeroController.php
+++ src/Controller/ContenuNumeroController.php
@@ -11,7 +11,48 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/contenu-numero')]
 class ContenuNumeroController extends AbstractController
 {
-    #[Route('/contenu/{id}', name: 'app_contenu_numero_list', methods: ['GET'])]
+    #[Route('/contenu_archive/{id}', name: 'app_contenu_numero_archive_list', methods: ['GET'])]
+    public function indexArchive(Contenu $contenu, \App\Repository\TexteRepository $texteRepository, \App\Repository\ArticleRepository $articleRepository): Response
+    {
+        // Fetch matching textes and articles by month/year of the Contenu's createdAt date
+        $date = $contenu->getCreatedAt();
+        $startOfMonth = (clone $date)->modify('first day of this month')->setTime(0, 0, 0);
+        $endOfMonth = (clone $date)->modify('last day of this month')->setTime(23, 59, 59);
+
+        $textes = $texteRepository->createQueryBuilder('t')
+            ->where('t.statut = :status')
+            ->andWhere('t.createdAt BETWEEN :start AND :end')
+            ->setParameter('status', \App\Entity\Texte::STATUT_PUBLISHED)
+            ->setParameter('start', $startOfMonth)
+            ->setParameter('end', $endOfMonth)
+            ->orderBy('t.createdAt', 'DESC')
+            ->getQuery()
+            ->getResult();
+
+        $articles = $articleRepository->createQueryBuilder('art')
+            ->where('art.statut = :status')
+            ->andWhere('art.createdAt BETWEEN :start AND :end')
+            ->setParameter('status', \App\Entity\Article::STATUT_PUBLISHED)
+            ->setParameter('start', $startOfMonth)
+            ->setParameter('end', $endOfMonth)
+            ->orderBy('art.createdAt', 'DESC')
+            ->getQuery()
+            ->getResult();
+
+        // affichage de tous les ContenuNumero d un contenu
+        return $this->render('contenu_numero/archive.html.twig', [
+            'contenu' => $contenu,
+            'contenu_numeros' => $contenu->getContenuNumeros()->filter(function($cn) {
+                return $cn->getStatut() === \App\Entity\ContenuNumero::STATUT_PUBLISHED;
+            }),
+            'contenu_discussions' => $contenu->getContenuDiscussions()->filter(function($cd) {
+                return $cd->getStatut() === \App\Entity\ContenuDiscussion::STATUT_PUBLISHED;
+            }),
+            'textes' => $textes,
+            'articles' => $articles,
+        ]);
+    }
+ #[Route('/contenu/{id}', name: 'app_contenu_numero_list', methods: ['GET'])]
     public function index(Contenu $contenu): Response
     {
         // affichage de tous les ContenuNumero d un contenu
@@ -22,7 +63,6 @@ class ContenuNumeroController extends AbstractController
             }),
         ]);
     }
-
     #[Route('/{id}/show', name: 'app_contenu_numero_show', methods: ['GET'])]
     public function show(ContenuNumero $contenuNumero): Response
     {
